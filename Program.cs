4b38f126259911c8183fa4d8ae325798573de41a
Williams Rogers Alfayo <willsrogersjr@gmail.com>
2021-05-21 14:58:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace helena_msumbufu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("willy ana kijicho!");
+            
+        }
+    }
+}
